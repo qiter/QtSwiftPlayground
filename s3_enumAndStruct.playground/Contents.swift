13108c2c26e770a166e350dec9ee7bc2bab67feb
qiter <qiter@qq.com>
2016-11-07 23:34:46 +0800

    update

---
+++ s3_enumAndStruct.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+//枚举和结构体
+
+//使用enum来创建一个枚举。就像类和其他所有命名类型一样，枚举可以包含方法
+
--- s3_objectAndClass.playground/Contents.swift
+++ s3_objectAndClass.playground/Contents.swift
@@ -73,19 +73,119 @@ test.area()
 test.simpleDescription()
 
 
+/*
+ 练习： 创建NamedShape的另一个子类Circle，构造器接收两个参数，一个是半径一个是名称，在子类Circle中实现area()和simpleDescription()方法。
+ */
+
+class Circle:ParentShape{
+    var r:Double = 5.0
+    
+    init(name: String,radius:Double) {
+        //super.name = name
+        self.r = radius
+        super.init(name: name)
+    }
+    
+    func area() -> Double{
+        return M_PI * self.r * self.r
+    }
+    
+    override func simpleDescription() -> String {
+        return "这个是一个圆圈类"
+    }
+    
+}
+
+/*除了储存简单的属性之外，属性可以有 getter 和 setter 。*/
+
+//等边三角形类
+
+class EquilateralTriangle:ParentShape{
+    
+    var sideLength:Double = 0.0
+    
+    init(sideLength:Double, name:String){
+        
+        self.sideLength = sideLength
+        super.init(name: name)
+        
+        numberOfSides = 3
+        
+    }
+    //周长
+    var perimeter:Double{
+        get{
+            return 3.0*sideLength
+        }
+        set{
+            sideLength = newValue/3.0
+        }
+    }
+    
+    override func simpleDescription() -> String {
+        return "等边三角形 边长为:\(self.sideLength)"
+    }
+    
+    
+}
+
+var triangle = EquilateralTriangle(sideLength: 3.1, name: "myTriangle")
+triangle.perimeter
+triangle.perimeter = 9.9
+triangle.sideLength
+
+triangle.name
 
 
+/*
+
+ 在perimeter的 setter 中，新值的名字是newValue。你可以在set之后显式的设置一个名字。
+ 
+ 注意EquilateralTriangle类的构造器执行了三步：
+ 
+ 设置子类声明的属性值
+ 调用父类的构造器
+ 改变父类定义的属性值。其他的工作比如调用方法、getters和setters也可以在这个阶段完成。
+ 如果你不需要计算属性，但是仍然需要在设置一个新值之前或者之后运行代码，使用willSet和didSet。
+ 
+ 比如，下面的类确保三角形的边长总是和正方形的边长相同。
+ 
+*/
+
+class TriangleAndSquare{
+    
+    var triangle:EquilateralTriangle{
+        willSet{
+            square.sideLength = newValue.sideLength
+        }
+    }
+    
+    
+    var square:Square{
+        willSet{
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+    
+    init(size:Double, name:String) {
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+    
+}
 
 
+var triangleAndSquare = TriangleAndSquare(size: 10, name: "myTestShape")
+triangleAndSquare.square.sideLength
+triangleAndSquare.triangle.sideLength
+triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+triangleAndSquare.triangle.sideLength
+/*
+处理变量的可选值时，你可以在操作（比如方法、属性和子脚本）之前加?。如果?之前的值是nil，?后面的东西都会被忽略，并且整个表达式返回nil。否则，?之后的东西都会被运行。在这两种情况下，整个表达式的值也是一个可选值。
+*/
 
-
-
-
-
-
-
-
-
+let optionalSquare:Square? = Square(sideLength: 2.5, name: "optionalSquare")
+let sideLength = optionalSquare?.sideLength
 
 
 
