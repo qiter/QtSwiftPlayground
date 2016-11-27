15cfd8318ac29c7af74b331b507b80d021b2502d
qiter <qiter@qq.com>
2016-11-27 23:33:28 +0800

    update

---
+++ s3_viewShow.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import Photos
+import PlaygroundSupport
+
+
+var view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+view.backgroundColor = UIColor.red
+PlaygroundPage.current.liveView = view
+
+
+
+
